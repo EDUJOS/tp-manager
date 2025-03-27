a28a9481805022e5ee295b8803bab0ae139af55b
Ed <josuegomo@hotmail.com>
2025-03-27 13:18:02 -0500

    Add player Position info to PlayerListPayload

--- src/main/java/zero/mods/fabric/tp4mods/payload/PlayerListPayload.java
+++ src/main/java/zero/mods/fabric/tp4mods/payload/PlayerListPayload.java
@@ -1,5 +1,6 @@
 package zero.mods.fabric.tp4mods.payload;
 
+import com.mojang.authlib.GameProfile;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.network.codec.PacketCodec;
 import net.minecraft.network.packet.CustomPayload;
@@ -10,31 +11,14 @@ import java.util.List;
 import java.util.UUID;
 import static zero.mods.fabric.tp4mods.Tp4Mods.MOD_ID;
 
-//public record PlayerListPayload(List<UUID> playerUuids) implements CustomPayload {
-//    public static PlayerListPayload fromPlayerInfos(List<PlayerInfoPayload> players) {
-//        return new PlayerListPayload(players.stream().map(PlayerInfoPayload::uuid).toList());
-//    }
-//
-//    public static final CustomPayload.Id<PlayerListPayload> ID =
-//            new CustomPayload.Id<>(Identifier.of(MOD_ID, "player_list"));
-//
-//    // Registro del códec (necesario para Fabric 1.21.4+)
-//    public static final PacketCodec<PacketByteBuf, PlayerListPayload> CODEC =
-//            PacketCodec.of(
-//                    (payload, buf) -> buf.writeCollection(payload.playerUuids(), (b, uuid) -> b.writeUuid(uuid)),
-//                    buf -> new PlayerListPayload(buf.readList(b -> b.readUuid()))
-//            );
-//
-//    @Override
-//    public Id<? extends CustomPayload> getId() {
-//        return ID;
-//    }
-//}
-
 public record PlayerListPayload(List<PlayerInfo> players) implements CustomPayload {
-    public record PlayerInfo(UUID uuid, String name) {
+    public record PlayerInfo(UUID uuid, String name, Position position) {
         public static PlayerInfo fromPlayer(ServerPlayerEntity player) {
-            return new PlayerInfo(player.getUuid(), player.getName().getString());
+            return new PlayerInfo(player.getUuid(), player.getName().getString(), Position.fromPlayer(player));
+        }
+
+        public GameProfile toGameProfile() {
+            return new GameProfile(uuid, name);
         }
     }
 
@@ -45,8 +29,9 @@ public record PlayerListPayload(List<PlayerInfo> players) implements CustomPaylo
             (payload, buf) -> buf.writeCollection(payload.players(), (b, info) -> {
                 b.writeUuid(info.uuid());
                 b.writeString(info.name());
+                info.position.write(b);
             }),
-            buf -> new PlayerListPayload(buf.readList(b -> new PlayerInfo(b.readUuid(), b.readString())))
+            buf -> new PlayerListPayload(buf.readList(b -> new PlayerInfo(b.readUuid(), b.readString(), Position.read(b))))
     );
 
     @Override
